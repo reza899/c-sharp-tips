68c783fe93720a02e59b64ab32de7622c504e500
Reza Bozorgi <reza.bozorgi@outlook.com>
2019-05-14 23:54:52 +0430

    func and action - delegate

--- c-sharp-tips/Program.cs
+++ c-sharp-tips/Program.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace c_sharp_tips
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // CollectionSample();
+            var p = new Program();
+            p.DelegateSample();
+            
+        }
+
+        private void DelegateSample()
+        {
+            Action<int> action = new Action<int>(DoSomething);
+            action(245);
+
+            Func<int, int, string> func = new Func<int, int, string>(DoSomethingCalc);
+            var ss= func(100, 240);
+        }
+
+        public string DoSomethingCalc(int i, int j)
+        {
+            return (i * j).ToString();
+        }
+        public void DoSomething(int i)
+        {
+            Console.WriteLine($"Do Something {i} times...");
+        }
+        private static void CollectionSample()
+        {
             int[] list = { 1, 4, 5, 6, 7 };
             ArrayList arlist = new ArrayList() { 1, 5, "Hello", 2.5 };
 
             IEnumerable<int> enumInt = list;
             var eee = enumInt.GetEnumerator();
-            int[] a=new int[5];
-            int j=0;
+            int[] a = new int[5];
+            int j = 0;
             foreach (var item in enumInt)
             {
                 eee.MoveNext();
@@ -39,10 +64,7 @@ namespace c_sharp_tips
             {
                 indx[i] = new Random().Next(100, 1000);
             }
-            Console.WriteLine(indx[90]); 
-            
+            Console.WriteLine(indx[90]);
         }
-
-
     }
 }
